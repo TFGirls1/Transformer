1ff74d190b93c62ec12c7273a9f6d38d01ffb6e5
longaadream <61306561+longaadream@users.noreply.github.com>
2025-11-02 03:05:18 +0800

    PDE Module(Primary Version)

--- TFdef.h
+++ TFdef.h
@@ -3,8 +3,8 @@
 #include <string_view>
 using uInt = __uint128_t;
 using Int = __int128_t;
-using ull = unsigned long long;
-using ll = long long;
+using ui64 = unsigned long long;
+using i64 = long long;
 using Char = char32_t;
 using String = std::u32string;
 using String_View = std::u32string_view;
