bcdfe9b7cb393027d46e56998cec9f41eda0d962
semika <xiezhilin69@gmail.com>
2025-11-02 17:53:04 +0800

    Update UnivStr.h

--- lib/UnivStr.h
+++ lib/UnivStr.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <codecvt>
 #include <string_view>
+#include <iostream>
 
 
 class UnivStr : public std::u32string {
@@ -54,4 +55,48 @@ inline UnivStr operator+(const UnivStr& lhs, char32_t rhs) {
 
 inline UnivStr operator+(char32_t lhs, const UnivStr& rhs) {
     return UnivStr(std::u32string(1, lhs) + static_cast<const std::u32string&>(rhs));
+}
+
+// Convert to UTF-8 for output
+inline std::string to_utf8(const UnivStr& str) {
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
+    return convert.to_bytes(str);
+}
+
+// Stream operators for UTF-8 input/output
+inline std::ostream& operator<<(std::ostream& os, const UnivStr& str) {
+    return os << to_utf8(str);
+}
+
+inline std::istream& operator>>(std::istream& is, UnivStr& str) {
+    std::string utf8_str;
+    is >> utf8_str;  // Read UTF-8 string
+    str = UnivStr(utf8_str);  // Convert to UTF-32 using existing constructor
+    return is;
+}
+
+// getline for reading full lines of UTF-8 text
+inline std::istream& getline(std::istream& is, UnivStr& str, char32_t delim = U'\n') {
+    std::string utf8_str;
+    // Read UTF-8 line and convert delimiter
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
+    char utf8_delim = convert.to_bytes(std::u32string(1, delim))[0];
+    
+    if (std::getline(is, utf8_str, utf8_delim)) {
+        str = UnivStr(utf8_str);
+    } else {
+        is.setstate(std::ios::failbit);
+    }
+    return is;
+}
+
+// Overload for char delimiters (common case for ASCII delimiters)
+inline std::istream& getline(std::istream& is, UnivStr& str, char delim) {
+    std::string utf8_str;
+    if (std::getline(is, utf8_str, delim)) {
+        str = UnivStr(utf8_str);
+    } else {
+        is.setstate(std::ios::failbit);
+    }
+    return is;
 }
